05418e03dd2fb7340be12ff2e98ec4dd810f2340
Cee-Jay Malacas <ceejaymalacas17@gmail.com>
2023-02-03 02:36:12 +0800

    style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
 import { useEffect, useState } from 'react'
+import dice from './assets/icon-dice.svg'
+import divider from './assets/pattern-divider-desktop.svg'
 
 function App() {
-  const [advice, setAdvice] = useState('')
+  const [advice, setAdvice] = useState("You can fail at what you don't want. So you might as well take a chance on doing what you love.")
+  const [adviceId, setAdviceId] = useState("185")
 
   const getAdvice = async () => {
-    try{
+    try {
       const res = await fetch('https://api.adviceslip.com/advice');
       const data = await res.json();
       setAdvice(data.slip.advice)
-      return data.slip.advice
-    }catch (err) {
+      setAdviceId(data.slip.id)
+    } catch (err) {
       console.log(err)
     }
   }
-  
+
   return (
     <div className="App">
-      <h1 className='text-3xl font-bold underline' onClick={getAdvice}> he{advice}</h1>
+      <div className="bg-graybg w-[500px] my-[300px] mx-auto px-[5px] pt-[15px] pb-[0px] text-center rounded-[10px]">
+        <p className="tracking-[5px] text-[10px] font-medium text-greenish">ADVICE #<span className="decoration-greenish">{adviceId}</span></p>
+        <p className="text-[25px] text-advc my-0 mx-[20px] pt-[5px] px-[35px] pb-[30px] leading-10 ">{advice}</p>
+        <div className="text-center flex justify-center">
+          <img src={divider} className="max-w-[100%]"/>
+        </div>
+        <div className="btn w-[60px] h-[60px] bg-btnc flex justify-center relative items-center left-[44%] top-[25px] transform-trans rounded-[50%] border-none pointer-events-auto">
+          <img className=" w-[24px] h-[24px]" onClick={getAdvice} src={dice} />
+        </div>
+      </div>
     </div>
   )
 }
 
 export default App
+
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'graybg': 'rgb(50, 58, 73)',
+        'advc': 'rgb(206, 227, 233)',
+        'greenish': 'rgb(82, 255, 168)',
+        'btnc': 'rgb(82, 255, 168)',
+      },
+      transformOrigin: {
+        'trans': 'translate(-50%, -50%)',
+      }
+    },
   },
   plugins: [],
 }
